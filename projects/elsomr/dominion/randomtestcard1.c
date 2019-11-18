94d17d230ceb17c51f6c92c028c40bb8ac67f04d
Robert Elsom <rcelsom@gmail.com>
2019-11-17 21:53:32 -0600

    fix randomtest2 syntax errors

--- projects/elsomr/dominion/randomtestcard1.c
+++ projects/elsomr/dominion/randomtestcard1.c
@@ -77,7 +77,7 @@ int main () {
     SelectStream(2);
     PutSeed(3);
 
-    for (n = 0; n < 2000; n++) {
+    for (n = 0; n < 10000; n++) {
         for (i = 0; i < sizeof(struct gameState); i++) {
             ((char*)&G)[i] = rand() * 256;
         }
