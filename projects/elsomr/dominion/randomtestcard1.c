fe34e2c34f5d48e91bfd1f82ff05f65024b2cf30
Robert Elsom <rcelsom@gmail.com>
2019-11-17 19:30:11 -0600

    fixed syntax in randomtestcard1

--- projects/elsomr/dominion/randomtestcard1.c
+++ projects/elsomr/dominion/randomtestcard1.c
@@ -15,31 +15,31 @@
 
 int checkBaronCard(int choice1, int Numplayers, int p, struct gameState *post) {
     struct gameState pre;
-	int failCounter = 0
+	int failCounter = 0;
     memcpy (&pre, post, sizeof(struct gameState));
 
     int r;
      printf ("Baron Effect PRE: p %d Coins %d HC %d DiC %d\n",
-    	  p, pre.coins[p], pre.handCount[p], pre.discardCount[p]);
+    	  p, pre.coins, pre.handCount[p], pre.discardCount[p]);
 
     r = baronEffect (choice1, post);
 
     printf ("Baron Effect POST: p %d Coins %d HC %d DiC %d\n\n\n",
-    	  p, post.coins[p], post.handCount[p], post.discardCount[p]);
+    	  p, post.coins, post.handCount[p], post.discardCount[p]);
 
 	if (choice1 > 0)	{
-		printf("Coin check = %s\n", assertEq(pre.coins, post.coins, failCounter));
-		printf("Hand Increases Check = %s\n", assertEq(pre.handCount[p]+1, post.handCount[p], failCounter));
-		printf("Card is put in discard pile check = %s\n", assertEq(estate, post.disard[p][post.discardCount[p]], failCount));
-		printf("Estate supply decreases = %s\n", assertEq(pre.supplyCount[estate], post.supplyCount[estate], failCounter));
+		printf("Coin check = %s\n", assertEq(pre.coins, post.coins, &failCounter));
+		printf("Hand Increases Check = %s\n", assertEq( pre.handCount[p]+1,  post.handCount[p], &failCounter));
+		printf("Card is put in discard pile check = %s\n", assertEq(estate, post.disard[p][post.discardCount[p]], &failCount));
+		printf("Estate supply decreases = %s\n", assertEq(pre.supplyCount[estate], post.supplyCount[estate], &failCounter));
 		printf("Note: only one of the last two tests should pass\n");
 		
 		
 	}
 	else if (choice1 == 0)	{
-		printf("Coin is static = %s\n", assertEq(pre.coins, post.coins, failCounter));
-		printf("Hand is static = %s\n", assertEq(pre.handCount[p], post.handCount[p], failCounter));
-		printf("Estate supply is static = %s\n", assertEq(pre.supplyCount[estate], post.supplyCount[estate], failCounter));
+		printf("Coin is static = %s\n", assertEq(pre.coins, post.coins, &failCounter));
+		printf("Hand is static = %s\n", assertEq(pre.handCount[p], post.handCount[p], &failCounter));
+		printf("Estate supply is static = %s\n", assertEq(pre.supplyCount[estate], post.supplyCount[estate], &failCounter));
 		
 	}
 	
@@ -71,7 +71,7 @@ int main () {
 		//pick a random choice (either 0 or 1)
 		choice1 = (int)floor(Random()) % 2;
 		//pick a random number of player(between 2 - 4)
-		numPlayers = (int)floor(Random()) % 3 + 2);
+		numPlayers = (int)floor(Random()) % 3 + 2;
 		//pick a random player
         p = floor(Random() % numPlayers);
 		//pick random deck, discard, and handcount values
@@ -79,14 +79,14 @@ int main () {
         G.discardCount[p] = (int)floor(Random()) % MAX_DECK;
         G.handCount[p] = (int)floor(Random()) % MAX_HAND;
 		//call check function
-        failCounter += checkBaronCard(choice1, numPlayers, player, &G);
+        failCounter += checkBaronCard(choice1, numPlayers, p, &G);
     }
 
 	if (failCounter == 0)	{
 		printf ("ALL TESTS OK\n");
 	}
 	else	{
-		printf( "%d TEST(s) FAILED\n");
+		printf( "%d TEST(s) FAILED\n", failCounter);
 	}
 
     exit(0);
