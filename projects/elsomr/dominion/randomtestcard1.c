886b3312ca0eabe1edeb8fcfc262d19d07b314a5
Robert Elsom <rcelsom@gmail.com>
2019-11-17 19:51:16 -0600

    fixed syntax in randomtestcard1

--- projects/elsomr/dominion/randomtestcard1.c
+++ projects/elsomr/dominion/randomtestcard1.c
@@ -30,28 +30,28 @@ int checkBaronCard(int choice1, int Numplayers, int p, struct gameState *post) {
 
 	if (choice1 > 0)	{
 		printf("Coin check = ");
-		printf("%s\n", assertEq(pre.coins, post->coins, &failCounter));
+		printf("%s\n", assertEq(pre.coins, post->coins, &failCounter, tmpResult));
 		printf("Hand Increases Check = ");
-		printf("%s\n", assertEq( pre.handCount[p]+1,  post->handCount[p], &failCounter));
+		printf("%s\n", assertEq( pre.handCount[p]+1,  post->handCount[p], &failCounter, tmpResult));
 		printf("Card is put in discard pile check = "); 
-		printf("%s\n", assertEq(estate, post->discard[p][post.discardCount[p]], &failCount));
+		printf("%s\n", assertEq(estate, post->discard[p][post.discardCount[p]], &failCount, tmpResult));
 		printf("Estate supply decreases = ");
-		printf("%s\n", assertEq(pre.supplyCount[estate], post->supplyCount[estate], &failCounter));
+		printf("%s\n", assertEq(pre.supplyCount[estate], post->supplyCount[estate], &failCounter, tmpResult));
 		printf("Note: only one of the last two tests should pass\n");
 		
 		
 	}
 	else if (choice1 == 0)	{
 		printf("Coin is static = ");
-		printf("%s\n", assertEq(pre.coins, post->coins, &failCounter));
+		printf("%s\n", assertEq(pre.coins, post->coins, &failCounter, tmpResult));
 		printf("Hand is static = ");
-		printf("%s\n", assertEq(pre.handCount[p], post->handCount[p], &failCounter));
+		printf("%s\n", assertEq(pre.handCount[p], post->handCount[p], &failCounter, tmpResult));
 		printf("Estate supply is static = ");
-		printf("%s\n", assertEq(pre.supplyCount[estate], post->supplyCount[estate], &failCounter));
+		printf("%s\n", assertEq(pre.supplyCount[estate], post->supplyCount[estate], &failCounter, tmpResult));
 		
 	}
 	
-	return failCounter
+	return failCounter;
 }
 
 int main () {
