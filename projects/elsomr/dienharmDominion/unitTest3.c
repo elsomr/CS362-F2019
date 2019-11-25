67f0b10dc843682f4adaa1155b4dc6f732ac7d21
Robert Elsom <rcelsom@gmail.com>
2019-11-24 21:02:00 -0600

    fixed typo in functino parameters

--- projects/elsomr/dienharmDominion/unitTest3.c
+++ projects/elsomr/dienharmDominion/unitTest3.c
@@ -70,7 +70,7 @@ int main() {
 					}
 				}
 				
-				ambassadorCardEffect(copper, 0, &G, 0);
+				ambassadorCardEffect(copper, 0, p, &G, 0);
 				
 				//count number of cards all other players have
 				for (i = 0; i < numPlayer; i++)	{
@@ -115,7 +115,7 @@ int main() {
 					}
 				}
 				
-				ambassadorCardEffect(copper, 1, &G, 0);
+				ambassadorCardEffect(copper, 0, p, &G, 0);
 				
 				//count number of cards all other players have
 				for (i = 0; i < numPlayer; i++)	{
@@ -157,7 +157,7 @@ int main() {
 					}
 				}
 				
-				ambassadorCardEffect(copper, 2, &G, 0);
+				ambassadorCardEffect(copper, 0, p, &G, 0);
 				
 				//count number of cards all other players have
 				for (i = 0; i < numPlayer; i++)	{
@@ -202,7 +202,7 @@ int main() {
 					}
 				}
 				
-				ambassadorCardEffect(copper, 2, &G, 0);
+				ambassadorCardEffect(copper, 0, p, &G, 0);
 				
 				//count number of cards all other players have
 				for (i = 0; i < numPlayer; i++)	{
@@ -245,7 +245,7 @@ int main() {
 					}
 				}
 				
-				ambassadorCardEffect(copper, 5, &G, 0);
+				ambassadorCardEffect(copper, 0, p, &G, 0);
 				
 				//count number of cards all other players have
 				for (i = 0; i < numPlayer; i++)	{
