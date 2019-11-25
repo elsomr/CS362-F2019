160b36ac6944046c997b57f4228fe1953ef68136
Robert Elsom <rcelsom@gmail.com>
2019-11-24 20:26:13 -0600

    fixed typo in make

--- projects/elsomr/dienharmDominion/unitTest2.c
+++ projects/elsomr/dienharmDominion/unitTest2.c
@@ -62,7 +62,7 @@ int main() {
 				}
 				
 				//gaining coins
-				minionCardEffect(1, 0, &G, handCount);
+				minionCardEffect(1, 0, p, &G, handCount);
 
 				
 				printf("Coin check = %d == %d", tempCoins+2, G.coins);
@@ -115,7 +115,7 @@ int main() {
 					}
 				}
 				
-				minionCardEffect(1, 0, &G, handCount);
+				minionCardEffect(1, 0, p, &G, handCount);
 				
 				printf("Coin check = %d == %d", tempCoins+2, G.coins);
 				assertEq(tempCoins+2, G.coins, failCounter, tmpResult);
