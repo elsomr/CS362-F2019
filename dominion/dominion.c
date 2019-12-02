3dfa0b4aa16e194af448ad6d1c30df92c16c2025
Robert Elsom <rcelsom@gmail.com>
2019-12-02 16:43:15 -0600

    test

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -1091,7 +1091,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         return 0;
 
     case ambassador:
-        j = 1;		//used to check if player has enough cards to discard,start at 1 for choice1
+        j = 0;		//used to check if player has enough cards to discard
 
         if (choice2 > 2 || choice2 < 0)
         {
@@ -1107,7 +1107,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         {
 			printf("\n\nout\n\n");
 			printf("%d  !=  %d  \n  %d == %d", i, handPos, i, state->hand[currentPlayer][choice1]);
-            if (i != handPos && state->hand[currentPlayer][i] == state->hand[currentPlayer][choice1] and i!= choice1)
+            if (i != handPos && state->hand[currentPlayer][i] == state->hand[currentPlayer][choice1])
             {
 				printf("\n\nin\n\n");
                 j++;
