0c15448437f4a778497405ce9b0a80180581b000
Robert Elsom <rcelsom@gmail.com>
2019-12-02 17:17:47 -0600

    test

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -688,9 +688,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     int k;
     int x;
     int index;
+	
     int currentPlayer = whoseTurn(state);
     int nextPlayer = currentPlayer + 1;
-	int tempCard;
+
     int tributeRevealedCards[2] = {-1, -1};
     int temphand[MAX_HAND];// moved above the if statement
     int drawntreasure=0;
@@ -1055,41 +1056,35 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
                 shuffle(nextPlayer,state);//Shuffle the deck
             }
             tributeRevealedCards[0] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
-            state->deck[nextPlayer][state->deckCount[nextPlayer]--] = -1;
-            // state->deckCount[nextPlayer]--;
-            tributeRevealedCards[1] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
-            state->deck[nextPlayer][state->deckCount[nextPlayer]--] = -1;
-            // state->deckCount[nextPlayer]--;
+            state->deck[nextPlayer][state->deckCount[nextPlayer]-1] = -1;
+            tributeRevealedCards[1] = state->deck[nextPlayer][state->deckCount[nextPlayer]-2];
+            state->deck[nextPlayer][state->deckCount[nextPlayer]-2] = -1;
+            state->deckCount[nextPlayer] = state->deckCount[nextPlayer]-2;
         }
 
-        // if (tributeRevealedCards[0] == tributeRevealedCards[1]) { //If we have a duplicate card, just drop one
-            // state->playedCards[state->playedCardCount] = tributeRevealedCards[1];
-            // state->playedCardCount++;
-            // tributeRevealedCards[1] = -1;
-        // }
-		
+        if (tributeRevealedCards[0] == tributeRevealedCards[1]) { //If we have a duplicate card, just drop one
+            state->playedCards[state->playedCardCount] = tributeRevealedCards[1];
+            state->playedCardCount++;
+            tributeRevealedCards[1] = -1;
+        }
 
-
-        for (i = 0; i < 2; i ++) {
+        for (i = 0; i <= 2; i ++) {
             if (tributeRevealedCards[i] == copper || tributeRevealedCards[i] == silver || tributeRevealedCards[i] == gold) { //Treasure cards
-
-				state->coins += 2;
+                state->coins += 2;
             }
 
             else if (tributeRevealedCards[i] == estate || tributeRevealedCards[i] == duchy || tributeRevealedCards[i] == province || tributeRevealedCards[i] == gardens || tributeRevealedCards[i] == great_hall) { //Victory Card Found
-
-				drawCard(currentPlayer, state);
+                drawCard(currentPlayer, state);
                 drawCard(currentPlayer, state);
             }
             else { //Action Card
                 state->numActions = state->numActions + 2;
-			}
+            }
         }
 
         return 0;
 
     case ambassador:
-		
         j = 0;		//used to check if player has enough cards to discard
 
         if (choice2 > 2 || choice2 < 0)
@@ -1104,8 +1099,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
         for (i = 0; i < state->handCount[currentPlayer]; i++)
         {
-			
-            if (i != handPos && state->hand[currentPlayer][i] == state->hand[currentPlayer][choice1])
+            if (i != handPos && i == state->hand[currentPlayer][choice1] && i != choice1)
             {
                 j++;
             }
@@ -1129,17 +1123,16 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
                 gainCard(state->hand[currentPlayer][choice1], state, 0, i);
             }
         }
-		tempCard = state->hand[currentPlayer][choice1];
 
         //discard played card from hand
         discardCard(handPos, currentPlayer, state, 0);
-		
+
         //trash copies of cards returned to supply
         for (j = 0; j < choice2; j++)
         {
             for (i = 0; i < state->handCount[currentPlayer]; i++)
             {
-                if (state->hand[currentPlayer][i] == tempCard)
+                if (state->hand[currentPlayer][i] == state->hand[currentPlayer][choice1])
                 {
                     discardCard(i, currentPlayer, state, 1);
                     break;
