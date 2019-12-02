02ff60a3de09989eb0914e93439dcb59a7051fd8
Robert Elsom <rcelsom@gmail.com>
2019-12-02 15:58:22 -0600

    test

--- dominion/unitTest9.c
+++ dominion/unitTest9.c
@@ -49,9 +49,8 @@ int main() {
    deckCount=2;
    //initialize next players hand
    G.deckCount[player+1] = 2;
-   G.deck[player+1][G.deckCount[nextPlayer]-1] = copper;
-   G.deck[player+1][G.deckCount[nextPlayer]-2] = copper;
-   G.deckCount[player+1] = 4
+   G.deck[player+1][G.deckCount[player+1]-1] = copper;
+   G.deck[player+1][G.deckCount[player+1]-2] = copper;
    G.coins = 0;
    G.numActions = 2;
 
