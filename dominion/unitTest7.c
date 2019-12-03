37682139615645d4725a0ab3fdf3912f9748a3ca
Robert Elsom <rcelsom@gmail.com>
2019-12-02 19:57:52 -0600

    unit7test add

--- dominion/unitTest7.c
+++ dominion/unitTest7.c
@@ -61,8 +61,8 @@ int main() {
    // show test result
    printf("Checking if Player 1's coins have increased by 2: ");
    custom_assert(G.coins == 2);
-   printf("Checking if Player 1's gains a card: %d == 2", G.handCount[player]);
-   custom_assert(G.handCount[player] == 2);
+   printf("Checking if Player 1's gains two cards: ");
+   custom_assert(G.handCount[player] == 4);
    printf("Checking if Player 1's number of actions is static ");
    custom_assert(G.numActions == 2);
 
